ba3c89e643de300a5c8fd25ac969390dbf6362cc
Henry <134236009+braindead-dev@users.noreply.github.com>
2024-11-18 15:35:34 -0800

    signup button

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,11 +2,22 @@
 
 import Link from "next/link";
 import { Shield } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
 import { UserAuthForm } from "@/app/login/components/user-auth-form";
 
 export default function LoginPage() {
   return (
     <div className="container relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <Link
+        href="/signup"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "absolute right-4 top-4 md:right-8 md:top-8"
+        )}
+      >
+        Sign up
+      </Link>
       <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
         <div className="absolute inset-0 bg-blue-600" />
         <div className="relative z-20 flex items-center text-lg font-medium">
