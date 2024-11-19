4d5827062fdbb3a4b3540974a0a4f9a94a603365
Henry <134236009+braindead-dev@users.noreply.github.com>
2024-11-19 15:13:07 -0800

    dashboard

--- components/dashboard/app-sidebar.tsx
+++ components/dashboard/app-sidebar.tsx
@@ -2,13 +2,10 @@
 
 import * as React from "react"
 import {
-  BookOpen,
+  Users,
   UserCog,
   Shield,
-  Frame,
   LifeBuoy,
-  Map,
-  PieChart,
   Send,
   FileChartColumnIncreasing,
   LayoutDashboard,
@@ -39,85 +36,21 @@ const data = {
       url: "#",
       icon: LayoutDashboard,
       isActive: true,
-      items: [
-        {
-          title: "History",
-          url: "#",
-        },
-        {
-          title: "Starred",
-          url: "#",
-        },
-        {
-          title: "Settings",
-          url: "#",
-        },
-      ],
     },
     {
       title: "Manage Profile",
       url: "#",
       icon: UserCog,
-      items: [
-        {
-          title: "Genesis",
-          url: "#",
-        },
-        {
-          title: "Explorer",
-          url: "#",
-        },
-        {
-          title: "Quantum",
-          url: "#",
-        },
-      ],
     },
     {
-      title: "Documentation",
+      title: "Manage Family",
       url: "#",
-      icon: BookOpen,
-      items: [
-        {
-          title: "Introduction",
-          url: "#",
-        },
-        {
-          title: "Get Started",
-          url: "#",
-        },
-        {
-          title: "Tutorials",
-          url: "#",
-        },
-        {
-          title: "Changelog",
-          url: "#",
-        },
-      ],
+      icon: Users,
     },
     {
       title: "Reports",
       url: "#",
       icon: FileChartColumnIncreasing,
-      items: [
-        {
-          title: "General",
-          url: "#",
-        },
-        {
-          title: "Team",
-          url: "#",
-        },
-        {
-          title: "Billing",
-          url: "#",
-        },
-        {
-          title: "Limits",
-          url: "#",
-        },
-      ],
     },
   ],
   navSecondary: [
