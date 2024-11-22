e34da04106acea1c71dcac10341813e09059ec60
Henry <134236009+braindead-dev@users.noreply.github.com>
2024-11-22 15:09:39 -0800

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -64,7 +64,7 @@ export default function LoginPage() {
         </div>
       </div>
       <div className="lg:p-8">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="mx-auto flex w-full max-w-sm flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
               Welcome back
